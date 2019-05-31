99095fc384cb8a5c06c71b17428b1d092174f5ed
Administrator <412050554@qq.com>
2019-05-31 21:50:49 +0800

    总结3sum问题

--- leetcode-node/target_sum/3sum_closest.py
+++ leetcode-node/target_sum/3sum_closest.py
@@ -8,37 +8,43 @@ class Solution(object):
         if len(nums) <=2:
             return -1
         if len(nums) == 3:
-            return abs(sum(nums) - target)
+            return sum(nums)
         nums_len = len(nums)
         i = 0
         nums.sort()
         sum_min = sum(nums[:3])
         sum_max = sum(nums[-3:])
         if target >= sum_max:
-            return target - sum_max
-        elif target <= sum_min:
-            return sum_min - target
-        diff_max = max([sum_max - target, target - sum_min])
+            # print "max"
+            return
 
-        while i < nums_len - 2 and nums[i] <= target:
-            #print diff_max
+        if  target <= sum_min:
+            # print "min"
+            return sum_min
+        diff_max = max([sum_max - target, target - sum_min])
+        ans = 0
+        while i < nums_len - 2:
+            # 由于是求绝对值最小，暂时无法推测3nums[i]与target的差值
             left = i + 1
             right = nums_len - 1
             while left < right:
-                diff = nums[i] + nums[left] + nums[right] - target
-                # print nums[i], nums[left], nums[right]
+                tmp = nums[i] + nums[left] + nums[right]
+                # print tmp
+                diff = tmp - target
                 if diff == 0:
-                    return 0
+                    return tmp
                 elif diff < 0:
                     diff = 0 - diff
                     if diff < diff_max:
                         diff_max = diff
+                        ans = tmp
                     left += 1
                 else:
                     if diff < diff_max:
                         diff_max = diff
+                        ans = tmp
                     right -= 1
             i += 1
-            while i < nums_len and nums[i-1] == nums[i]:
-                i += 1
-        return diff_max
+            # while i < nums_len and nums[i-1] == nums[i]:
+            #     i += 1
+        return ans
